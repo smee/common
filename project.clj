d7af023d3294cf686e73b3fca16d7c4940b3bd13
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2012-05-07 09:24:04 +0200

    [release] bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.smee/common "1.2.4"
-  :description "FIXME: write description"
+(defproject org.clojars.smee/common "1.2.5"
+  :description "Common useful functions needed in different projects."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  ])
