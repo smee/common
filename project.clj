5e1ae39372f43ba7a009b5861db72ac1732bb6db
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2013-07-31 13:16:44 +0200

    [feature] process/shell commands handling. This allows comfortable consuming of process standard outputs

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.smee/common "1.2.7-SNAPSHOT"
+(defproject org.clojars.smee/common "1.2.8-SNAPSHOT"
   :description "Common useful functions needed in different projects."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/smee/common"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 ]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [conch "0.3.0"]]
   :pom-addition [:developers
                  [:developer [:name "Steffen Dienst"]]]
             )
