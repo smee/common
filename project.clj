0b993c0427f8d1eb7513928daf60142247f40c79
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2015-04-16 10:24:53 +0200

    Version 1.2.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.smee/common "1.2.8-SNAPSHOT"
+(defproject org.clojars.smee/common "1.2.8"
   :description "Common useful functions needed in different projects."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
